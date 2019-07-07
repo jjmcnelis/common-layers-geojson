336ae84241957eb410b263667a260081c2d83d4c
Jack McNelis <jjmcnelis@outlook.com>
2019-07-07 03:01:42 -0400

    fix

---
+++ README.md
@@ -0,0 +1,3 @@
+# README.md
+
+Run the shell script to download TIGER shapefiles from Census.gov and convert to geojson in WGS84 with `ogr2ogr`. Must have gdal/ogr binary tools installed: `sudo apt-get install gdal-bin` if you're on ubuntu.
--- gen-geojson.sh
+++ gen-geojson.sh
@@ -1,73 +1,55 @@
+#!/bin/bash
 
-#############################################################################
-# US 
+echo ##### US #####
 
-# # boundary
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_nation_5m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_nation_20m.zip
+echo -- downloading
 
-# # city
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_09_concity_500k.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_13_concity_500k.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_18_concity_500k.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_20_concity_500k.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_21_concity_500k.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_30_concity_500k.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_47_concity_500k.zip
+echo boundary
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_nation_5m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_nation_20m.zip
 
-# # county
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_5m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_20m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_500k.zip
+echo county
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_5m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_20m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_500k.zip
 
-# # division
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_division_5m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_division_20m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_division_500k.zip
+echo division
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_division_5m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_division_20m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_division_500k.zip
 
-# # region
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_region_5m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_region_20m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_region_500k.zip
+echo region
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_region_5m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_region_20m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_region_500k.zip
 
-# # state
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_5m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_20m.zip
-# curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_500k.zip
+echo state
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_5m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_20m.zip
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_500k.zip
 
+echo -- making geojsons
 
-#############################################################################################
-# make geojsons
-
-# boundary
+echo boundary
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_nation_5m.json /vsizip/cb_2018_us_nation_5m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_nation_20m.json /vsizip/cb_2018_us_nation_20m.zip
 
-# city
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_09_concity_500k.json /vsizip/cb_2018_09_concity_500k.zip
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_13_concity_500k.json /vsizip/cb_2018_13_concity_500k.zip
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_18_concity_500k.json /vsizip/cb_2018_18_concity_500k.zip
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_20_concity_500k.json /vsizip/cb_2018_20_concity_500k.zip
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_21_concity_500k.json /vsizip/cb_2018_21_concity_500k.zip
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_30_concity_500k.json /vsizip/cb_2018_30_concity_500k.zip
-ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_47_concity_500k.json /vsizip/cb_2018_47_concity_500k.zip
-
-# county
+echo county
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_county_5m.json /vsizip/cb_2018_us_county_5m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_county_20m.json /vsizip/cb_2018_us_county_20m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_county_500k.json /vsizip/cb_2018_us_county_500k.zip
 
-# division
+echo division
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_division_5m.json /vsizip/cb_2018_us_division_5m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_division_20m.json /vsizip/cb_2018_us_division_20m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_division_500k.json /vsizip/cb_2018_us_division_500k.zip
 
-# region
+echo region
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_region_5m.json /vsizip/cb_2018_us_region_5m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_region_20m.json /vsizip/cb_2018_us_region_20m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_region_500k.json /vsizip/cb_2018_us_region_500k.zip
 
-# state
+echo state
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_state_5m.json /vsizip/cb_2018_us_state_5m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_state_20m.json /vsizip/cb_2018_us_state_20m.zip
 ogr2ogr -f geojson -t_srs EPSG:4326 cb_2018_us_state_500k.json /vsizip/cb_2018_us_state_500k.zip
