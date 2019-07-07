acb1f3380fffd7c2cc0c70907fe429d27cfb9d51
Jack McNelis <jjmcnelis@outlook.com>
2019-07-07 03:44:02 -0400

    reduce

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# README.md
+# get common vector data
 
-Run the shell script to download TIGER shapefiles from Census.gov and convert to geojson in WGS84 with `ogr2ogr`. Must have gdal/ogr binary tools installed: `sudo apt-get install gdal-bin` if you're on ubuntu.
+run the shell script `gen-geojson.sh` to make new json files from [US census](#) and [natural earth](#) data:
+```shell
+./gen-geojson.sh
+```
+
+must have gdal/ogr binary tools installed. if you're on ubuntu:
+```
+sudo apt-get install gdal-bin
+``` 
+---
+The script does the following:
+
+1. downloads tiger shapefiles from census.gov 
+
+```shell
+curl -O https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_nation_5m.zip
+```
+
+2. converts to geojson in `-t_srs EPSG:4326` with `ogr2ogr`, reducing vertices (douglas-peucker) by factor `-simplify 0.001`
+
+```shell
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_nation_5m.json /vsizip/cb_2018_us_nation_5m.zip
+```
--- gen-geojson.sh
+++ gen-geojson.sh
@@ -31,26 +31,25 @@ echo -- downloading
 echo -- making geojsons
 
 echo boundary
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_nation_5m.json /vsizip/cb_2018_us_nation_5m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_nation_5m.json /vsizip/cb_2018_us_nation_5m.zip
 ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_nation_20m.json /vsizip/cb_2018_us_nation_20m.zip
 
 echo county
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_county_5m.json /vsizip/cb_2018_us_county_5m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_county_20m.json /vsizip/cb_2018_us_county_20m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_county_500k.json /vsizip/cb_2018_us_county_500k.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_county_5m.json /vsizip/cb_2018_us_county_5m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_county_20m.json /vsizip/cb_2018_us_county_20m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_county_500k.json /vsizip/cb_2018_us_county_500k.zip
 
 echo division
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_division_5m.json /vsizip/cb_2018_us_division_5m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_division_20m.json /vsizip/cb_2018_us_division_20m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_division_500k.json /vsizip/cb_2018_us_division_500k.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_division_5m.json /vsizip/cb_2018_us_division_5m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_division_20m.json /vsizip/cb_2018_us_division_20m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_division_500k.json /vsizip/cb_2018_us_division_500k.zip
 
 echo region
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_region_5m.json /vsizip/cb_2018_us_region_5m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_region_20m.json /vsizip/cb_2018_us_region_20m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_region_500k.json /vsizip/cb_2018_us_region_500k.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_region_5m.json /vsizip/cb_2018_us_region_5m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_region_20m.json /vsizip/cb_2018_us_region_20m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_region_500k.json /vsizip/cb_2018_us_region_500k.zip
 
 echo state
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_state_5m.json /vsizip/cb_2018_us_state_5m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_state_20m.json /vsizip/cb_2018_us_state_20m.zip
-ogr2ogr -f geojson -simplify 0.01 -t_srs EPSG:4326 cb_2018_us_state_500k.json /vsizip/cb_2018_us_state_500k.zip
-
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_state_5m.json /vsizip/cb_2018_us_state_5m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_state_20m.json /vsizip/cb_2018_us_state_20m.zip
+ogr2ogr -f geojson -simplify 0.001 -t_srs EPSG:4326 cb_2018_us_state_500k.json /vsizip/cb_2018_us_state_500k.zip
